d7010b3719835dca71d654256120f387180d2257
Hossam <7osssam@protonmail.ch>
2023-10-25 00:59:26 +0300

    update KEYPAD.c and KEYPAD.h

--- AVR Interfacing/03-HAL/KEYPAD.h
+++ AVR Interfacing/03-HAL/KEYPAD.h
@@ -19,8 +19,6 @@
  *                                Definitions                                  *
  *******************************************************************************/
 
-#define STANDARD_KEYPAD
-
 /* Keypad configurations for number of rows and columns */
 #define KEYPAD_NUM_COLS		   4
 #define KEYPAD_NUM_ROWS		   4
@@ -42,13 +40,23 @@
 #define KEYPAD_BUTTON_PRESSED  LOGIC_LOW
 #define KEYPAD_BUTTON_RELEASED LOGIC_HIGH
 
+#define KEYPAD_NO_PRESSED_KEY  0xFF
+
 /*******************************************************************************
  *                      Functions Prototypes                                   *
  *******************************************************************************/
 
-/*
- * Description :
- * Get the Keypad pressed button
+/**
+ * @brief Get the pressed key from the keypad.
+ *
+ * This function scans the rows and columns of the keypad to detect a keypress.
+ * When a key is pressed, it returns the corresponding key value, or KEYPAD_NO_PRESSED_KEY
+ * if no key is currently pressed. It also sets up the keypad row and column pins
+ * for scanning and resets their directions to input after scanning.
+ *
+ * @return The value of the pressed key (0-9, '*', '#', '+', '-', '/', 'C'), or KEYPAD_NO_PRESSED_KEY if no key is pressed.
+ * 
+ * @note This function can be used as a blocking function, or it can be called periodically to check for a keypress.
  */
 uint8 KEYPAD_getPressedKey(void);
 
