1be860ba00d7586c224639e11c457c833158ea8e
Hossam <7osssam@protonmail.ch>
2023-10-25 18:21:30 +0300

    update Calculator.h

--- AVR Interfacing/APP/Calculator/Calculator.h
+++ AVR Interfacing/APP/Calculator/Calculator.h
@@ -1,4 +1,14 @@
-
+/******************************************************************
+ * @Project	 	: Calculator  
+ * @Description : A simple calculator with 2 modes (Basic and Programmer). 
+ * 				  The calculator supports the following operations:	
+ * 				  1. Basic Mode: +, -, *, /, %, =, AC, DEL (with precedence and associativity).
+ * 				  2. Programmer Mode: DEC -> BIN and BIN.
+ * @Note 		: The calculator handles Invalid inputs and operations.
+ * @target  	: ATMEGA32 (eta32)
+ * @Author 	 	: Hossam Mohamed
+ * @Compiler 	: avr-gcc   
+ ********************************************************************/
 #ifndef __CALCULATOR_H__
 #define __CALCULATOR_H__
 
