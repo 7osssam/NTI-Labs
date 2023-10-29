391b64c52b39ebe8a512aabc31b94aec3e96ce14
Hossam <7osssam@protonmail.ch>
2023-10-29 13:38:05 +0200

    update ADC_App.c

--- AVR Interfacing/APP/ADC_ProgressBar/ADC_App.c
+++ AVR Interfacing/APP/ADC_ProgressBar/ADC_App.c
@@ -1,8 +1,10 @@
 
 #include "ADC_App.h"
 
+#include "DIO.h"
 #include "LCD.h"
 #include "ADC.h"
+#include "EXTI.h"
 #include "ProgressBar.h"
 
 #include "Buzzer.h"
@@ -12,16 +14,21 @@
 #include "SETTINGS.h"
 #include <util/delay.h>
 
+#define LDR_FLAG			 0
+#define LM35_FLAG			 1
+
 /* Maximum temperature that can be measured by the sensor */
 #define LM35_MAX_TEMPERATURE 150
 /* the voltage of the sensor at the maximum temperature */
 #define LM35_MAX_VOLT		 1.5
 
 // buffer for ADC values to multiple channels
-uint16 g_ADC_arr_value[2] = {0, 0};
+volatile uint16 g_ADC_arr_value[2] = {0, 0};
 
-uint8 temp_value = 0;
-uint8 LDR_value = 0;
+volatile uint8 temp_value = 0;
+volatile uint8 LDR_value = 0;
+
+volatile uint8 g_ADC_flag = 2; // 2 means no interrupt has occurred yet
 
 void ADC_ISR(void)
 {
@@ -31,24 +38,29 @@ void ADC_ISR(void)
 
 	// g_ADC_arr_value[0] --> LDR
 	// g_ADC_arr_value[1] --> LM35
+	LDR_value = g_ADC_arr_value[LDR_FLAG];
 
-	LDR_value = g_ADC_arr_value[0];
-
-	temp_value = (uint8)(((uint32)g_ADC_arr_value[1] * LM35_MAX_TEMPERATURE * ADC_REF_VOLT_VALUE) /
+	temp_value = (uint8)(((uint32)g_ADC_arr_value[LM35_FLAG] * LM35_MAX_TEMPERATURE * ADC_REF_VOLT_VALUE) /
 						 (ADC_MAXIMUM_VALUE * LM35_MAX_VOLT));
 
-	// start conversion for the next channel
-	ADC_startConversionNonBlocking(i);
-
-	if (temp_value >= 30 || LDR_value >= 100)
+	if (LDR_value >= 100)
 	{
-		Buzzer_on();
+		DIO_SetPinValue(PORTD_ID, PIN2_ID, LOGIC_HIGH); // Trigger External Interrupt 0
+		g_ADC_flag = LDR_FLAG; // save the channel number that triggered the interrupt
+	}
+	else if (temp_value >= 30)
+	{
+		DIO_SetPinValue(PORTD_ID, PIN2_ID, LOGIC_HIGH); // Trigger External Interrupt 0
+		g_ADC_flag = LM35_FLAG; // save the channel number that triggered the interrupt
 	}
 	else
 	{
-		Buzzer_off();
+		DIO_SetPinValue(PORTD_ID, PIN2_ID, LOGIC_LOW);
+		g_ADC_flag = 2;
 	}
 
+	// start conversion for the next channel
+	ADC_startConversionNonBlocking(i);
 	i++;
 	// reset i to 0 if it reaches 2
 	if (i == 2)
@@ -57,39 +69,58 @@ void ADC_ISR(void)
 	}
 }
 
+void EXTI0_ISR(void)
+{
+	Buzzer_on();
+	_delay_ms(1000);
+	Buzzer_off();
+}
+
 void ADC_APP(void)
 {
-	uint16 ADC_value = 0;
-
 	ADC_ConfigType ADC_config = {
 		.ref_volt = ADC_AVCC, .prescaler = ADC_PRESCALER_128, .auto_trigger_source = ADC_FREE_RUNNING_MODE};
+	EXTI_ConfigType EXTI0_config = {.interrupt = EXTI_INT0, .sense_control = EXTI_rising_edge};
+
+	EXTI_setCallBack(EXTI_INT0, ADC_ISR);
+	ADC_setCallback(ADC_ISR);
 
 	ADC_init(&ADC_config);
 
+	EXTI_init(&EXTI0_config);
+
 	LCD_init();
-	ProgressBar_init();
 
 	ADC_startConversionNonBlocking(ADC0); // LDR
 	ADC_startConversionNonBlocking(ADC1); // LM35
 
 	ADC_InterruptEnable();
-	ADC_setCallback(ADC_ISR);
+	EXTI_enable(&EXTI0_config);
 
 	sei(); // Enable Global Interrupts
 
 	while (1)
 	{
-		LCD_Goto_XY(0, 0);
-		LCD_displayString("LDR = ");
-		LCD_displayInteger(LDR_value);
-		LCD_displayString("     ");
-
-		LCD_Goto_XY(1, 0);
-		LCD_displayString("Temp = ");
-		LCD_displayInteger(temp_value);
-		LCD_displayString("     ");
-
-		ProgressBar(ADC_MAXIMUM_VALUE, g_ADC_arr_value[0], 2);
-		ProgressBar(ADC_MAXIMUM_VALUE, g_ADC_arr_value[1], 3);
+		if (g_ADC_flag != 2)
+		{
+			LCD_Goto_XY(0, 0);
+			if (g_ADC_flag == LDR_FLAG)
+			{
+				LCD_displayString("LDR is  ");
+				LCD_displayInteger(LDR_value);
+				ProgressBar(ADC_MAXIMUM_VALUE, LDR_value, 2);
+			}
+			else if (g_ADC_flag == LM35_FLAG)
+			{
+				LCD_displayString("LM35 is ");
+				LCD_displayInteger(temp_value);
+				ProgressBar(LM35_MAX_TEMPERATURE, temp_value, 2);
+			}
+		}
+		else
+		{
+			LCD_Goto_XY(0, 0);
+			LCD_displayString("No DATA        ");
+		}
 	}
 }
