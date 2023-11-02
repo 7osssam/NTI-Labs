69c9561490955f9dde2ae765d60b2b55b0a1e9b9
Hossam <7osssam@protonmail.ch>
2023-11-02 16:33:47 +0200

    update Sensor.c

--- AVR Interfacing/03-HAL/Sensor.c
+++ AVR Interfacing/03-HAL/Sensor.c
@@ -22,12 +22,9 @@ uint8 LM35_Read(void)
 	uint8  temp_value = 0;
 	uint16 ADC_value = 0;
 
-	//0.54931640625
-
 	/* Read ADC channel where the temperature sensor is connected */
 	ADC_value = ADC_readChannelBlocking(LM35_CH);
 
-	//
 	/* Calculate the temperature from the ADC value*/
 	temp_value = (uint8)(((uint32)ADC_value * LM35_MAX_TEMPERATURE * ADC_REF_VOLT_VALUE) /
 						 (ADC_MAXIMUM_VALUE * LM35_MAX_VOLT));
