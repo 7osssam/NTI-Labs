30af5136b32f5a71afad1831e8c27ae8a9aca501
Hossam <7osssam@protonmail.ch>
2023-11-09 17:50:55 +0200

    update GPIO_reg.h

--- ARM/STM32F103C8T7/Inc/GPIO_reg.h
+++ ARM/STM32F103C8T7/Inc/GPIO_reg.h
@@ -36,5 +36,10 @@
 #define PIN8			  (8)
 #define PIN9			  (9)
 #define PIN10			  (10)
+#define PIN11			  (11)
+#define PIN12			  (12)
+#define PIN13			  (13)
+#define PIN14			  (14)
+#define PIN15			  (15)
 
 #endif /* GPIO_REG_H_ */
